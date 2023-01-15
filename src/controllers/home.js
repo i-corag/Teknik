d3407a8146761502d8a57e1da3b9ac444fd70033
i-corag <icoraglio@gmail.com>
2023-01-15 17:54:26 +0100

    feat: style contact and messageReceived

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,8 @@
+const {teamMembers} = require('../conceptual/workteam.js');
+
 
 const sendHomeView = (req , res) => {
-    return res.render('pages/home.ejs');
+    return res.render('pages/home.ejs', {teamMembers});
 };
 
 module.exports = {sendHomeView};
