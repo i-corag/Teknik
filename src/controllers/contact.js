0a2ee47a2430ef8fd09086fb451cafef0cc55388
letoor1234 <angelmartinezwb@gmail.com>
2023-03-14 19:24:42 -0300

    chore: environment vars sample file

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -1,11 +1,9 @@
-
 const sendContactForm = (req, res) => {
-    return res.render('pages/contact.ejs');
+  return res.render("pages/contact.ejs", { baseUrl: process.env.BASE_URL });
 };
 
 const sendReceived = (req, res) => {
-    return res.render('pages/received.ejs');
-}
-
+  return res.render("pages/received.ejs");
+};
 
 module.exports = { sendContactForm, sendReceived };
